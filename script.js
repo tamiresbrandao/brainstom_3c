9d6042ccb14d79b136be35e67032d287738f2d92
tamiresbrandao <167766003+tamiresbrandao@users.noreply.github.com>
2026-08-20 09:47:36 -0300

    s

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-constcaixaprincipal = document.queryselector("caixa-principal");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+
+   {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+
+   {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+
+   {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+
+   {
+    enunciado: "Pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+  {
+    enunciado: "Pergunta 2",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+  },
+];
